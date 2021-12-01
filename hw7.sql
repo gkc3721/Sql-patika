292b86ad06a1febfd142b276b28ddf93da27be69
gkc3721 <91606113+gkc3721@users.noreply.github.com>
2021-12-01 14:38:08 +0300

    Update hw7.sql

--- hw7.sql
+++ hw7.sql
@@ -54,3 +54,9 @@ select concat(first_name, ' ', last_name) as "isim-soyisim" from actor
 select active,count(active) from customer
 group by active
 
+--Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. 
+--If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+select name from students
+where marks>75
+ORDER BY RIGHT(Name,3), ID;
+--The RIGHT() function extracts a number of characters from a string (starting from right)
